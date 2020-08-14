7774c5ff619d14a9230203fa61246ed20e80edfd
Rui de Alencar Frota <ruidealencar@gmail.com>
2020-08-14 14:45:18 -0300

    add info

--- rescue_else_ensure/example_1.rb
+++ rescue_else_ensure/example_1.rb
@@ -3,3 +3,9 @@
 rescue ZeroDivisionError
   puts '<undividable>'
 end
+
+# Ruby 2.4
+# gera um erro quando tentamos usar rescue/else/garantir dentro dos blocos do/end
+
+# Ruby 2.5
+# suporta rescue/else/ensure dentros dos blocos do/end
